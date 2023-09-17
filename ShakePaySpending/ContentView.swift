2736d74836457f4ffb9c6fb7d685acec6c7ca8f3
needMDMA <parties01smacks@icloud.com>
2023-09-17 14:18:50 -0400

    Initial Commit

---
+++ ShakePaySpending/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShakePaySpending
+//
+//  Created by Olivier Lambert Rouillard on 2023-09-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShakePaySpending/ShakePaySpendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakePaySpendingApp.swift
+//  ShakePaySpending
+//
+//  Created by Olivier Lambert Rouillard on 2023-09-17.
+//
+
+import SwiftUI
+
+@main
+struct ShakePaySpendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
